170802243dad696f3b0d5d31e08a1b0279bba278
Minkon Kim <alsrhs98@gmail.com>
2023-09-27 12:38:45 +0900

    시큐리티 config 작성

--- src/main/java/com/cos/security1/controller/IndexController.java
+++ src/main/java/com/cos/security1/controller/IndexController.java
@@ -2,6 +2,7 @@ package com.cos.security1.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller // View를 리턴하겠다.
 public class IndexController {
@@ -12,4 +13,33 @@ public class IndexController {
         //view 리졸버 설정: template (prefix), .mustache(suffix) 생략가능함요  - 의존성만 등록하면 그만이야
         return "index";
     }
+    @GetMapping("/user")
+    public String user(){
+        return "user";
+    }
+
+    @GetMapping("/admin")
+    public String admin(){
+        return "admin";
+    }
+
+    @GetMapping("/manager")
+    public String manager(){
+        return "manager";
+    }
+    @GetMapping("/login")
+    public String login(){
+        return "loginForm";
+    }
+    @GetMapping("/join")
+    public String join(){
+        return "join";
+    }
+
+    @GetMapping("/joinProc")
+    public @ResponseBody String joinProc(){
+        return "회원가입 완료됨";
+    }
+
+
 }
